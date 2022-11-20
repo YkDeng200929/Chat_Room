227629c28f315ff6abc662b25f9cd44454d7a770
Ykdeng_Win <dmtmd520@hotmail.com>
2022-11-20 19:04:28 +0800

    20221120功能函数

--- chat_room/fun.c
+++ chat_room/fun.c
@@ -4,26 +4,31 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <signal.h>
 #include "head.h"
 
+/*初始化*/
 void init(c_list *info_book)
 {
-    bzero(info_book, sizeof(c_list));
+    info_book->cfd = -1;
+    
     info_book->next = NULL;
 }
 
+/*登录*/
 bool s_login(int connect_fd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr)
 {
     /*登录用户数据包*/
     pack * temp = client_pack;
     c_list *new_info = NULL;
-    new_info->addr = client_addr->sin_addr.s_addr;
+    new_info->addr.sin_addr.s_addr = client_addr->sin_addr.s_addr;
     new_info->cfd = connect_fd;
     new_info->next = NULL;
 
@@ -32,6 +37,7 @@ bool s_login(int connect_fd, pack * client_pack, c_list * info_book, struct sock
     info_book->next = new_info;
 }
 
+/*聊天*/
 bool s_chat(int connect_fd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr)
 {
     c_list *curse = NULL;
@@ -41,5 +47,22 @@ bool s_chat(int connect_fd, pack * client_pack, c_list * info_book, struct socka
     }
 }
 
-bool s_logout(int connect_fd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr)
-{}
+/*登出*/
+bool s_logout(int closefd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr)
+{
+    c_list *find = NULL;
+    c_list *prev = NULL;
+    for (prev = info_book, find = prev->next; find != NULL || find->cfd != closefd; prev = prev->next, find = prev->next)
+    {}
+    if (find == NULL)
+        return false;
+    prev->next = find->next;
+    free(find);
+    return true;
+}
+
+/*回收子进程*/
+void signal_handler(int signum)
+{
+    waitpid(-1, NULL, WNOHANG);
+}
--- chat_room/head.h
+++ chat_room/head.h
@@ -7,17 +7,20 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
 
 #define LOG_IN 1
-#define LOG_OUT 0
 #define CHAT 2
+#define LOG_OUT 3
 
+/*登录用户信息表*/
 typedef struct c_list
 {
     int cfd;
@@ -25,6 +28,7 @@ typedef struct c_list
     struct c_list *next;
 }c_list;
 
+/*客户端用户信息包*/
 typedef struct pack
 {
     int type;
@@ -33,15 +37,18 @@ typedef struct pack
 }pack;
 
 /*初始化用户信息册*/
-void init(c_list *info_book);
+extern void init(c_list *info_book);
 
 /*负责添加用户信息到 client_book*/
-extern bool s_login(int connect_fd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr)
+extern bool s_login(int connect_fd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr);
 
 /*负责传送用户信息并广播到所有人*/
-extern bool s_chat(int connect_fd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr)
+extern bool s_chat(int connect_fd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr);
 
 /*负责关闭离线客户端后删除 client_book 中的离线用户信息*/
-extern bool s_logout(int connect_fd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr)
+extern bool s_logout(int connect_fd, pack * client_pack, c_list * info_book, struct sockaddr_in * client_addr);
+
+/*等待回收僵尸子进程*/
+extern void signal_handler(int signum);
 
 #endif
