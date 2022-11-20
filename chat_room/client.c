c5594daa17fa1b6186116a43ca41f34daa142212
Ykdeng_Win <dmtmd520@hotmail.com>
2022-11-20 19:04:40 +0800

    无法传送结构体

--- chat_room/client.c
+++ chat_room/client.c
@@ -2,6 +2,7 @@
 
 int main(int argc, const char *argv[])
 {
+    printf("client\n");
     int ret = -1;
     pack msg;
     if (argc < 3)
@@ -9,27 +10,36 @@ int main(int argc, const char *argv[])
         printf("Please enter ip && port\n");
         return -1;
     }
+
+    /*1. 创建套接字*/
     int cfd = socket(AF_INET, SOCK_STREAM, 0);   
     if (cfd == 0)
     {
         perror("socket");
         return 0;
     }
+
+    /*2. 创建连接服务端信息*/
     struct sockaddr_in saddr;
     bzero(&saddr, sizeof(saddr));
     saddr.sin_family = AF_INET;
     saddr.sin_addr.s_addr = inet_addr(argv[1]);
     saddr.sin_port = (htons(atoi(argv[2])));
 
+    /*3. 发送连接请求*/
     ret = connect(cfd, (struct sockaddr *)&saddr, sizeof(saddr));
     if (ret < 0)
     {
         perror("connect");
         return -1;
     }
+
+    /*4. 通信缓存*/
     char buf[1024] = {0};
     char name[10] = {0};
     int option = -1;
+
+    /*5. 通信*/
     while (1)
     {
         if (option == LOG_IN)
@@ -70,15 +80,18 @@ int main(int argc, const char *argv[])
         {
             printf("1) log in\n");
             printf("2) chat\n");
-            printf("3) log in\n");
-            printf("Please enter options(q to quit): ");
+            printf("3) log out\n");
+            printf("Please enter options: ");
             scanf("%d", &option);
             getchar();
         }
         while (option < 0 || option > 3);
 
-        printf("Enter your name: ");
-        fgets(name, sizeof(name), stdin);
+        if (option == 1)
+        {
+            printf("Enter your name: ");
+            fgets(name, sizeof(name), stdin);
+        }
     }
 
     return -1;
